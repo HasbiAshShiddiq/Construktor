2bd7b795e5bac64d2a9c5b3a3188eab9e4d093eb
HasbiAshShiddiq <abi.hasbiashshiddiq@gmail.com>
2019-04-02 14:59:49 +0700

    Constructor

---
+++ Mahasiswa.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Constuctor
+{
+    class Mahasiswa
+    {
+        // Constructor Default
+        public Mahasiswa()
+        {
+
+        }
+
+        //Constructor Dengan 2 Parameter
+        public Mahasiswa(string nim, string nama)
+        {
+            Nim = nim;
+            Nama = nama;
+        }
+
+        //Properties
+        public string Nim { get; set; }
+        public string Nama { get; set; }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Constuctor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Membuat Objek mhs1
+            Mahasiswa mhs1 = new Mahasiswa();
+            CetakHasil(mhs1);
+
+            Console.WriteLine();//Baris Kosong
+
+            //Membuas objek mhs2
+            Mahasiswa mhs2 = new Mahasiswa("12345", "Paijo");
+            CetakHasil(mhs2);
+
+            Console.ReadKey();
+        }
+        static void CetakHasil(Mahasiswa mhs)
+        {
+            Console.WriteLine("Nim : {0}", mhs.Nim);
+            Console.WriteLine("Nama : {0}", mhs.Nama);
+        }
+    }
+}
